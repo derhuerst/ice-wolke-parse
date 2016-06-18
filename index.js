e556e3ae52b48a4062c9edd36bfce663774909b7
Jannis R <mail@jannisr.de>
2016-06-18 13:35:17 +0200

    bugfixes :bug, group by line

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ndjson = require('ndjson')
 const filter = require('stream-filter')
 const sink = require('stream-sink')
 const maxBy = require('lodash.maxBy')
+const groupBy = require('lodash.groupBy')
 
 const cfg = require('./package.json').config
 const center = require('./geographic-center')
@@ -22,9 +23,9 @@ const splitIntoTrips = () => {
 			((d.when - last) > 60 * 60 || d.line !== line)) {
 			this.push(trip)
 			trip = []
-			line = d.line
 		} else trip.push(d)
 		last = d.when
+		line = d.line
 		cb()
 	}, function (cb) {
 		this.push(trip)
@@ -51,35 +52,36 @@ const timeslice = () => map.obj((trip) => {
 
 
 got.stream(cfg.wolke)
-.on('error', console.error)
 .pipe(ndjson.parse())
-.on('error', console.error)
-.pipe(filter((data) => data.line === 'ICE 123'))
 .pipe(splitIntoTrips())
 .pipe(sort())
 .pipe(relative())
 .pipe(timeslice())
 .pipe(sink({objectMode: true}))
 .on('data', (trips) => {
+	const tripsByLine = groupBy(trips, (trip) => trip[0].line)
+	const normalized = {}
+	for (let line in tripsByLine) {
+		const trips = tripsByLine[line]
 
-	// Because we want to get proper average positions by time, we have to right-pad faster (shorter) trips.
-	const longest = maxBy(trips, (trip) => Object.keys(trip).length)
-	const maxLength = Object.keys(longest).length
+		// Because we want to get proper average positions by time, we have to right-pad faster (shorter) trips.
+		const longest = maxBy(trips, (trip) => Object.keys(trip).length)
+		const maxLength = Object.keys(longest).length
 
-	for (let trip of trips) {
-		const length = Object.keys(trip).length
-		const last = trip[length - 1]
-		for (let i = length; i < maxLength; i++) trip[i] = last
+		for (let trip of trips) {
+			const length = Object.keys(trip).length
+			const last = trip[length - 1]
+			for (let i = length; i < maxLength; i++) trip[i] = last
+		}
+
+		// Compute average positions by time
+		const centers = normalized[line] = []
+		for (let i = 0; i < maxLength; i++) {
+			const position = center(trips.map((trip) => trip[i]))
+			centers.push(Object.assign(position, {relative: i * cfg.interval}))
+		}
+
 	}
-
-	// Compute average positions by time
-	const centers = []
-	for (let i = 0; i < maxLength; i++) {
-		const position = center(trips.map((trip) => trip[i]))
-		centers.push(Object.assign(position, {
-			line:     trips[0][0].line,
-			relative: i * cfg.interval
-		}))
-	}
+	process.stdout.write(JSON.stringify(normalized))
 
 })
